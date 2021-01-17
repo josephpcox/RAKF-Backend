7653821fe5bcdc041643073edc275ccbd331ff98
John Cameron <jcameron2@mail.usf.edu>
2021-01-16 20:15:06 -0500

    Add Event model

--- app/resources/models.py
+++ app/resources/models.py
@@ -4,9 +4,14 @@ from sqlalchemy import exc
 db = SQLAlchemy()
 
 class User(db.Model):
-    __tablename__ = 'Users'
+    __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(320), unique=True, nullable=False)
     password = db.Column(db.String(255), nullable=False)
     first_name = db.Column(db.String(50), nullable=False)
     last_name = db.Column(db.String(50), nullable=False)
+
+class Event(db.Model):
+    __tablename__ = 'events'
+    id = db.Column(db.Integer, primary_key=True)
+    event_name = db.Column(db.String(255), nullable=False)
