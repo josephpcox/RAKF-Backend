972a9371213d3c000a432d1add0d4b5b9d0a3eae
John Cameron <jcameron2@mail.usf.edu>
2021-01-20 22:32:23 -0500

    Update permission db schema

--- app/resources/models.py
+++ app/resources/models.py
@@ -2,47 +2,56 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import exc
 from datetime import datetime
 
+from sqlalchemy.orm import relationship, backref
+
 db = SQLAlchemy()
+
 
 class User(db.Model):
     __tablename__ = 'users'
     user_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    group_id = db.Column(db.Integer, nullable=False, unique=True)
     first_name = db.Column(db.String(50), nullable=False)
     last_name = db.Column(db.String(50), nullable=False)
     email = db.Column(db.String(320), unique=True, nullable=False)
     password = db.Column(db.String(255), nullable=False)
-    is_admin = db.Column(db.Boolean, default=False, nullable=False, unique=False)
     deleted = db.Column(db.Boolean, default=False, nullable=True, unique=False)
-    update_date = db.Column(db.DateTime, default=datetime.utcnow())  
+    update_date = db.Column(db.DateTime, default=datetime.utcnow())
     created_date = db.Column(db.DateTime, default=datetime.utcnow())
-    permission_membership = db.relationship('PremissionMembership', lazy='dynamic')
+    user_id_fk = relationship("permission_membership.user_id", backref="parent", passive_deletes=True)
+
 
 class PermissionMembership(db.Model):
-    __tablename__='permission_membership'
-    pm_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    user_id = db.Column(db.Integer,db.ForeignKey('users.user_id'),nullable=False)
-    group_id = db.Column(db.Integer,nullable=False)
+    __tablename__ = 'permission_membership'
+    user_id = db.Column(db.Integer, db.ForeignKey('users.user_id', ondelete='CASCADE'), nullable=False,
+                        primary_key=True)
+    group_id = db.Column(db.Integer, db.ForeignKey('permission_group_meta.group_id', ondelete='CASCADE'),
+                         nullable=False, primary_key=True)
+
 
 class PermissionGroupMeta(db.Model):
     __tablename__ = 'permission_group_meta'
-    group_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    group_id = db.Column(db.Integer, nullable=False, primary_key=True, autoincrement=True)
     name = db.Column(db.String(50), nullable=False)
-    priority = db.Column(db.Integer, default=0,nullable=True)
+    priority = db.Column(db.Integer, nullable=True, default=0)
+    group_id_fk = relationship("permission_entries.group_id", backref="parent", passive_deletes=True)
+    group_id_fk_2 = relationship("permission_membership.group_id", backref="parent", passive_deletes=True)
+
 
 class PermissionEntries(db.Model):
     __tablename__ = 'permission_entries'
-    pe_id = db.Column(db.Integer, primary_key=True, autoincrement=True) 
-    is_group = db.Column(db.Boolean,default=False, unique=False)
-    permission = db.Column(db.String(50), nullable=False)
-    value = db.Column(db.Boolean, default=True,unique=False)
+    group_id = db.Column(db.Integer, db.ForeignKey('permission_group_meta.group_id', ondelete='CASCADE'),
+                         nullable=False, primary_key=True)
+    permission = db.Column(db.String(50), nullable=False, primary_key=True)
+    value = db.Column(db.Boolean, nullable=False, default=True)
+
 
 class Fish(db.Model):
-    __tablename__= 'fish'
-    fish_id = db.Column(db.Integer, primary_key=True,autoincrement=True)
+    __tablename__ = 'fish'
+    fish_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     fishName = db.Column(db.String(320), unique=True, nullable=False)
+
 
 class Event(db.Model):
     __tablename__ = 'events'
-    event_id = db.Column(db.Integer, primary_key=True,autoincrement=True)
+    event_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     event_name = db.Column(db.String(255), nullable=False)
