5d881d8b65540f333352aa037e30072f3d8dde39
Joseph Cox <joseph.p.coxbb22@gmail.com>
2021-06-21 22:45:50 -0400

    got a working dockerfile for deployment

--- wsgi.py
+++ wsgi.py
@@ -11,4 +11,4 @@ if __name__=="__main__":
         load_dotenv('dev.env')
         from app.server import create_app
         app=create_app(config_name="local")
-        app.run(port=5000, debug=True)
+        app.run(host="0.0.0.0", port=5000, debug=True)
